59a1ce827ef28021084ef5bd1c7fc5c38c302d0b
gmgercke <82462147+gmgercke@users.noreply.github.com>
2024-02-01 10:22:36 +0100

    Module Division

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./Header";
 import MainPart from "./MainPart";
 import Footer from "./Footer";
---
+++ src/MainPart.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./MainPart.css";
+
+export default function MainPart() {
+
+
+}
