d4c967308327860849f2cb2d4b2ffe05f10c3750
gmgercke <82462147+gmgercke@users.noreply.github.com>
2024-02-01 10:29:26 +0100

    Debugging

--- src/Footer.js
+++ src/Footer.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./Footer.css";
 
 export default function Footer() {
-
+    return (
+        <div className="Footer">
+          <p>Hallo Test</p>
+        </div>
+      );
 
 }
--- src/MainPart.js
+++ src/MainPart.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./MainPart.css";
 
 export default function MainPart() {
-
-
+    return (
+        <div className="MainPart">
+          <p>Hallo Test</p>
+        </div>
+      );
 }
